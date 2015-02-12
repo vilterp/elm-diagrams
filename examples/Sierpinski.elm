00a0012ea69dcc4d1113adf6ec8700502a47048b
Pete Vilter <pete.vilter@gmail.com>
2015-02-11 20:23:12 -0600

    style of primitives now specified in terms of FillStroke type -- combo of FillStyle & LineStyle

--- examples/Sierpinski.elm
+++ examples/Sierpinski.elm
@@ -8,18 +8,18 @@ import Time
 import Window
 
 sierpinski n sl = case n of
-                    0 -> eqTriangle sl C.defaultLine
+                    0 -> eqTriangle sl (justFill <| C.Solid Color.blue)
                     _ -> let smaller = sierpinski (n-1) sl
                          in smaller `above`
                             (alignCenter <| smaller `beside` smaller)
 
 -- not spinning
---main = fullWindowMain <| alignCenter <| sierpinski 3 20
+main = fullWindowMain <| alignCenter <| sierpinski 3 20
 
-framesPast = Signal.foldp (\_ st -> st + 1) 0 (Time.fps 30)
-framesPerRotation = 100
-angle = Signal.map (\past -> ((toFloat <| past % framesPerRotation) / framesPerRotation) * (2*pi)) framesPast
+--framesPast = Signal.foldp (\_ st -> st + 1) 0 (Time.fps 30)
+--framesPerRotation = 100
+--angle = Signal.map (\past -> ((toFloat <| past % framesPerRotation) / framesPerRotation) * (2*pi)) framesPast
 
 -- spinning
-main = let sier = showOrigin <| alignCenter <| sierpinski 5 20
-       in Signal.map2 (\wh ang -> fullWindowView wh <| rotate ang sier) Window.dimensions angle
+--main = let sier = showOrigin <| alignCenter <| sierpinski 5 20
+--       in Signal.map2 (\wh ang -> fullWindowView wh <| rotate ang sier) Window.dimensions angle
