bd440f9ca21210aef9c325c98ba26fd7a78217a4
Pete Vilter <pete.vilter@gmail.com>
2015-12-06 00:14:04 -0800

    actually add type module

---
+++ Diagrams/Type.elm
@@ -0,0 +1,23 @@
+module Diagrams.Type where
+
+import Graphics.Element as E
+import Graphics.Collage as C
+import Text as T
+
+import Diagrams.FillStroke exposing (..)
+import Diagrams.Geom exposing (..)
+import Diagrams.Actions exposing (..)
+
+type Diagram t a
+    -- primitives
+    = Circle Float FillStroke
+    | Rect Float Float FillStroke
+    | Polygon (List Point) FillStroke
+    | Path (List Point) C.LineStyle
+    | Text String T.Style E.Element
+    -- transformation
+    | TransformD Transform (Diagram t a)
+    -- group
+    | Group (List (Diagram t a))
+    -- tag
+    | Tag t (ActionSet t a) (Diagram t a)
